625d72cee8fbcae5dc60b25ca54fe7d3884f81b6
Peter Lauer <lauerpeter11@gmail.com>
2023-10-10 18:05:51 -0500

    update-check

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,29 +18,19 @@ const initdb = async () =>
 
 
 export const putDb = async (content) => {
-  // First, create a variable, and set it to asynchronously await the opening of the database.
   const db = await openDB(DB_NAME, 1);
-  // Now create a transaction for read-write access.
   const tx = db.transaction(DB_NAME, 'readwrite');
-  // Create a variable for the object store.
   const store = tx.objectStore(DB_NAME);
   await store.put({ id: 1, value: content });
-  // wait for transaction to complete
   await new Promise((resolve) => tx.addEventListener('complete', resolve));
-
   console.log('🚀 - data saved to the database:', content);
 };
 
 export const getDb = async () => {
-
   const db = await openDB(DB_NAME, 1);
   const tx = db.transaction(DB_NAME, 'readonly');
   const store = tx.objectStore(DB_NAME);
-
-  // retrieve content from the object store 
   const request = store.get(1); 
-
-  // await transaction to complete
   const result = await new Promise((resolve) => {
     request.addEventListener('success', () => resolve(request.result));
     request.addEventListener('error', () => resolve(null));
